846b134cc854b375cb83cf29e8e4d153e7dabbfc
Jackzmwon <92358047+Jackzmwon@users.noreply.github.com>
2021-11-06 11:19:20 +0530

    Update git.js

--- plugins/git.js
+++ plugins/git.js
@@ -24,7 +24,7 @@ Asena.addCommand({pattern: 'git', fromMe: false, desc: Lang.WP}, (async (message
 *wizard ser wa.me/+917306103794?text=Hi*
 *bot made by jackz*
 *coding jackz*
-*special thankz to wizard and anjali*
+*special thankz to wizard and anjali and paruzz*
 
 *www.instagram.com/safar_muhmd*
 
